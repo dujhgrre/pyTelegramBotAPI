a69ad1405956425faf09e68350fc55ce3dc67485
dujhgrre <eimakoto725@gmail.com>
2025-03-16 15:21:02 +0100

    Update reply_keyboard_markup_example.py

--- examples/reply_keyboard_markup_example.py
+++ examples/reply_keyboard_markup_example.py
@@ -6,6 +6,7 @@ TOKEN = "<your_token>"
 bot = telebot.TeleBot(TOKEN)
 
 keys = ["1","2","3","4","5","6","7","8","9","0","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+kekys = ["1","2","3","4","5","6","7","8","9","0","й","ц","у","к","е","н","г","ш","щ","з","х","ъ","ф","ы","в","а","п","р","о","л","д","ж","э","я","ч","с","м","и","т","ь","б","ю"]
 symbols = ["1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","\'","\"","/","\\",",",".",";",":"]
 
 def keyboard(key_type="Normal"):
@@ -19,7 +20,19 @@ def keyboard(key_type="Normal"):
         markup.add(*row)
         row = [KeyboardButton(x) for x in keys[29:]]
         markup.add(*row)
-        markup.add(KeyboardButton("Caps Lock"),KeyboardButton("Symbols"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"))
+        markup.add(KeyboardButton("Caps Lock"),KeyboardButton("Symbols"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"),KeyboardButton("русский"))
+    elif key_type == "русский":
+        row = [KeyboardButton(x) for x in kekys[:10]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in kekys[10:20]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in kekys[20:30]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in kekys[30:39]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in kekys[39:]]
+        markup.add(*row)
+        markup.add(KeyboardButton("Caps Lock"),KeyboardButton("Symbols"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"),KeyboardButton("русский"))
     elif key_type == "Symbols":
         row = [KeyboardButton(x) for x in symbols[:10]]
         markup.add(*row)
@@ -27,8 +40,9 @@ def keyboard(key_type="Normal"):
         markup.add(*row)
         row = [KeyboardButton(x) for x in symbols[20:]]
         markup.add(*row)
-        markup.add(KeyboardButton("Caps Lock"),KeyboardButton("Normal"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"))
+        markup.add(KeyboardButton("Caps Lock"),KeyboardButton("Normal"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"),KeyboardButton("русский"))
     else:
+        print(key_type)
         row = [KeyboardButton(x.upper()) for x in keys[:10]]
         markup.add(*row)
         row = [KeyboardButton(x.upper()) for x in keys[10:20]]
@@ -37,12 +51,14 @@ def keyboard(key_type="Normal"):
         markup.add(*row)
         row = [KeyboardButton(x.upper()) for x in keys[29:]]
         markup.add(*row)
-        markup.add(KeyboardButton("Normal"),KeyboardButton("Symbols"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"))
+        markup.add(KeyboardButton("Normal"),KeyboardButton("Symbols"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"),KeyboardButton("русский"))
     return markup
+
+
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id,"You can use the keyboard",reply_markup=keyboard())
+    bot.send_message(message.chat.id,"можете пользоваться клавиатуру",reply_markup=keyboard())
 
 @bot.message_handler(func=lambda message:True)
 def all_messages(message):
@@ -57,6 +73,8 @@ def all_messages(message):
         bot.send_message(message.from_user.id,"Caps Lock",reply_markup=keyboard("Caps"))
     elif message.text == "🔙Delete":
         bot.delete_message(message.from_user.id,message.message_id)
+    elif message.text == "русский":
+        bot.send_message(message.from_user.id,"Русская клавиатура",reply_markup=keyboard("русский"))
     else:
         bot.send_message(message.chat.id,message.text)
 
